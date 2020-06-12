6fa7487620987a35b00a1d43a8c7ec5e3ec911c8
Hugo Brito <hgbrito.tr@gmail.com>
2020-06-12 11:05:09 -0300

    Criação do projeto SpringBoot backend

---
+++ backend/lembrar/src/main/java/br/com/hgpbrito/lembrar/LembrarApplication.java
@@ -0,0 +1,13 @@
+package br.com.hgpbrito.lembrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembrarApplication.class, args);
+	}
+
+}
---
+++ backend/lembrar/src/test/java/br/com/hgpbrito/lembrar/LembrarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hgpbrito.lembrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
